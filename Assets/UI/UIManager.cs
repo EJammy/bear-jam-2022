56111765a2493058cd5591465e1726cdf8d53f55
Lena Jeon <bluewood900@gmail.com>
2022-10-29 22:00:49 -0700

    Small UI tweak

--- Assets/UI/UIManager.cs
+++ Assets/UI/UIManager.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private void onEnable() {
+        var VE_Root = GetComponent<UIDocument>().rootVisualElement;
 
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 }

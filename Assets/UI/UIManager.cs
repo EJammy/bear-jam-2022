1ffa79f1533384b622522c4fbb93c9dc119b9813
Lena Jeon <bluewood900@gmail.com>
2022-10-29 22:40:34 -0700

    Fireball assets done

--- Assets/UI/UIManager.cs
+++ Assets/UI/UIManager.cs
@@ -8,6 +8,11 @@ public class UIManager : MonoBehaviour
     private void onEnable() {
         var VE_Root = GetComponent<UIDocument>().rootVisualElement;
 
-        
+        //How to get elements:
+        tile1 = VE_Root.Q<GroupBox>("Tile1"); //Name in quotes is found in the UI Builder
+
+        //Different events:
+        somebutton.RegisterCallback<ClickEvent>(ev => FunctionCall());
+
     }
 }
